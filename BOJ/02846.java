673d0f5714e27ec4f4b9b4e81a0c166745e05196
JAEEUN JANG <33060748+Jeje01@users.noreply.github.com>
2020-02-10 00:11:20 +0900

    Update 02846.java

--- BOJ/02846.java
+++ BOJ/02846.java
@@ -5,23 +5,25 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] arr = new int[n];
-		int i, sum=0, max=0;
+		int i, gap=0, start=0, end=0, max=0;
 		for(i=0; i<n; i++) {
 			arr[i]= sc.nextInt();
 		}
 		for(i=0; i<n-1; i++) {
 			if(arr[i]<arr[i+1]) {
-				if(sum!=0)
-					sum+=arr[i+1];
+				if(start==0) {
+					start=arr[i];
+					end=arr[i+1];
+				}
 				else
-					sum=arr[i]+arr[i+1];
+					end=arr[i+1];
 			}
 			else {
-				max = max<sum? sum: max;
-				sum=0;
+				gap=end-start;
+				max = max<gap? gap: max;
+				gap=0;
 			}
 		}
-		System.out.println(max);
-		
+		System.out.println(max);	
 	}	
 }
