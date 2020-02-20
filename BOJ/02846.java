11774a06994b5b1d2e9c6b6216db5ef6f3c7d881
JAEEUN JANG <33060748+Jeje01@users.noreply.github.com>
2020-02-21 00:07:50 +0900

    Update 02846.java

--- BOJ/02846.java
+++ BOJ/02846.java
@@ -1,29 +1,36 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Main {
+public class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int i, start=0, up=0, max=0;
 		int[] arr = new int[n];
-		int i, gap=0, start=0, end=0, max=0;
+		ArrayList<Integer> list = new ArrayList<>();
 		for(i=0; i<n; i++) {
-			arr[i]= sc.nextInt();
+			arr[i] = sc.nextInt();
 		}
-		for(i=0; i<n-1; i++) {
-			if(arr[i]<arr[i+1]) {
-				if(start==0) {
-					start=arr[i];
-					end=arr[i+1];
+		for(i=1; i<n; i++) {
+			if(arr[i-1]<arr[i]) {
+				if(up==0) {
+					start=arr[i-1];
+					up=1;
 				}
-				else
-					end=arr[i+1];
 			}
 			else {
-				gap=end-start;
-				max = max<gap? gap: max;
-				gap=0;
+				if(up==1) {
+					list.add(arr[i-1]-start);
+					up=0;
+				}
 			}
 		}
-		System.out.println(max);	
-	}	
+		if(up==1) {
+			list.add(arr[n-1]-start);
+		}	
+		for(int a: list) {
+			max = max<a? a: max;
+		}
+		System.out.println(max);
+	}
 }
