d6ed7eaeed05d821e13d5666ef9d9c410e85340f
JAEEUN JANG <33060748+Jeje01@users.noreply.github.com>
2020-03-21 00:27:34 +0900

    Create 11722.java

---
+++ BOJ/11722.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        int i, j, answer=0;
+        for(i=1; i<n+1; i++){
+            arr[i] = sc.nextInt();
+        }
+        dp[1] = 1;
+        for(i=2; i<n+1; i++){
+            dp[i] = 1;
+            for(j=1; j<i; j++){
+                if(arr[j] > arr[i] && dp[j]>=dp[i]){
+                    dp[i] = dp[j]+1;
+                } else if(arr[j] == arr[i]){
+                    dp[i] = dp[j];
+                }
+            }
+        }
+        for(i=1; i<n+1; i++){
+            if(dp[i] > answer) answer = dp[i];
+        }
+        System.out.println(answer);
+    }
+}
