f1a49fe9a6b60c05ab324ae1433d10ec8d1481e4
JAEEUN JANG <jje141@gmail.com>
2020-04-26 23:19:45 +0900

    Update 05101.java

--- BOJ/05101.java
+++ BOJ/05101.java
@@ -1,30 +1,32 @@
-import java.util.Scanner;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a, b, c;
-		List<Integer> x = new ArrayList<>();
-		while(true) {
-			a = sc.nextInt();
-			b = sc.nextInt();
-			c = sc.nextInt();
-			if(!(a==0&&b==0&&c==0)) {
-				if((c-a)%b==0)
-					x.add((c-a)/b+1);
-				else
-					x.add(0);
-			}
-			else
-				break;
-		}	
-		for(int i : x) {
-			if(i==0)
-				System.out.println("X");
-			else
-				System.out.println(i);
-		}
-	  
-	}
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ArrayList<String> list = new ArrayList<>();
+        while(true){
+            String[] sp = br.readLine().split(" ");
+            int a = Integer.parseInt(sp[0]);
+            int b = Integer.parseInt(sp[1]);
+            int c = Integer.parseInt(sp[2]);
+            if(a==0 && b==0 && c==0) break;
+            if((c-a)%b==0)
+                if((c-a)*b<0)
+                    list.add("X");
+                else
+                    list.add(((c-a)/b+1)+"");
+            else
+                list.add("X");
+        }
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).equals("X"))
+                bw.write("X\n");
+            else
+                bw.write(list.get(i)+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
 }
