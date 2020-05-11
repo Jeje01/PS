de3a2b8a5896141289ab4a20f38e870d6fe8238f
JAEEUN JANG <jje141@gmail.com>
2020-05-11 23:56:44 +0900

    Create 12947.java

---
+++ programmers/level1/12947.java
@@ -0,0 +1,13 @@
+//하샤드 수
+
+public class HarshadNumber{
+    public boolean isHarshad(int num){
+    String[] temp = String.valueOf(num).split("");
+    int sum = 0;
+    for (String s : temp)
+        sum += Integer.parseInt(s);
+    if (num % sum == 0)
+        return true;
+    else
+        return false;
+}
