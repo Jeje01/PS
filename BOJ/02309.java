9a85cb2e2fbee1703395ad7146d6ab55393084f6
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-07-29 20:24:16 +0900

    Update 02309 using combination

--- BOJ/02309.java
+++ BOJ/02309.java
@@ -1,28 +1,44 @@
+/**
+ * 일곱 난쟁이
+ * 조합
+ * 14324 kb
+ * 116 ms
+ */
+
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
+    static int[] dwarfs, pick;
+    static int sum;
+    static void combination(int r, int start, int[] temp) {
+        if(r==0) {
+            if(temp[0]+temp[1]==sum-100) {
+                pick[0]=temp[0];
+                pick[1]=temp[1];
+            }
+            return;
+        }
+        for(int i=start; i<dwarfs.length; i++) {
+            temp[r-1]=dwarfs[i];
+            combination(r-1, i+1, temp);
+        }
+    }
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[] inp = new int[9];
-        int i, j, sum=0, a=0, b=0;
+        dwarfs = new int[9];
+        int i, j, a=0, b=0;
+        sum=0;
         for(i=0; i<9; i++) {
-            inp[i] = sc.nextInt();
-            sum+=inp[i];
+            dwarfs[i] = sc.nextInt();
+            sum+=dwarfs[i];
         }
-        Arrays.sort(inp);
-        loop:
-        for(i=0; i<9; i++){
-            for(j=0; j<9; j++){
-                if(i==j) continue;
-                if(inp[i]+inp[j]==sum-100){
-                    a = i; b = j;
-                    break loop;
-                }
-            }
-        }
+        pick = new int[2];
+        combination(2, 0, new int[2]);
+        Arrays.sort(dwarfs);
         for(i=0; i<9; i++)
-            if(i!=a && i!=b)
-                System.out.println(inp[i]);
+            if(dwarfs[i]!=pick[0] && dwarfs[i]!=pick[1])
+                System.out.println(dwarfs[i]);
     }
 }
