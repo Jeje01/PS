52b7c1b622fb16ba6cfcd74c7d82bc145f718f69
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-08-26 23:35:51 +0900

    Update 02206.java

--- BOJ/02206.java
+++ BOJ/02206.java
@@ -5,8 +5,10 @@
  * 904 ms
  * 
  * 주의할 점
- * 벽을 깬 경로와 깨지 않은 경로 충돌 가능 -> 깬 경우
- * 깨지 않은 경우 visited를 따로 관리해줘야 함
+ * 벽을 깬 경로와 깨지 않은 경로 충돌 가능 
+ * -> 깬 경우, 깨지 않은 경우 visited를 따로 관리해줘야 함 
+ * -> 3차원 visited 배열 사용
+ * 
  * 그럼 priority queue 필요 없음
  */
  
@@ -106,3 +108,89 @@ public class Main {
 	}
 
 }
+
+/**
+ * 200826
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static int N, M, answer;
+	static boolean[][][] visited;
+	static int[][] map;
+	static Queue<Pos> queue = new LinkedList<>();
+	static int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+	
+	static class Pos{
+		int r;
+		int c;
+		int broke;
+		int dis;
+		
+		public Pos(int r, int c, int broke, int dis) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.broke = broke;
+			this.dis = dis;
+		}
+		
+
+	}
+	
+	public static void main(String[] args) throws IOException{
+		tokens = new StringTokenizer(input.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
+		map = new int[N][M];
+		answer = Integer.MAX_VALUE;
+
+		visited = new boolean[N][M][2];
+		for(int i=0; i<N; i++) {
+			String line = input.readLine();
+			for(int j=0; j<M; j++) {
+				map[i][j] = line.charAt(j)-'0';
+			}
+		}
+		queue.offer(new Pos(0, 0, 0, 1));
+		visited[0][0][0] = true;
+		while(!queue.isEmpty()) {
+			Pos tmp = queue.poll();
+			if(tmp.r==N-1 && tmp.c==M-1) {
+				answer = Math.min(answer, tmp.dis);
+			}
+			
+			for(int i=0; i<dirs.length; i++) {
+				int nr = tmp.r+dirs[i][0];
+				int nc = tmp.c+dirs[i][1];
+				if(nr>=0 && nr<map.length && nc>=0 && nc<map[0].length) {
+					if(tmp.broke==1 && map[nr][nc]==1) continue;
+					if(map[nr][nc]==1) {
+						if(!visited[nr][nc][1]) {
+							queue.offer(new Pos(nr, nc, 1, tmp.dis+1));
+							visited[nr][nc][1] = true;
+						}
+					}else {
+						if(!visited[nr][nc][tmp.broke]) {
+							queue.offer(new Pos(nr, nc, tmp.broke, tmp.dis+1));
+							visited[nr][nc][tmp.broke] = true;
+						}
+					}
+				}
+			}
+		}
+		
+		answer = answer==Integer.MAX_VALUE? -1: answer;
+		System.out.println(answer);
+	}
+		
+}
+
+ */
