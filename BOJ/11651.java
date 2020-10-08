db2feaf471465136973515561f58673b3989a8d2
JAEEUN JANG <jje141@gmail.com>
2020-10-08 07:44:40 -1000

    [S5] 좌표 정렬하기 2 - 정렬

---
+++ BOJ/11651.java
@@ -0,0 +1,41 @@
+/**
+ * 좌표 정렬하기 2 - S5
+ * 정렬
+ * 75,124 kb
+ * 780 ms
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+   static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+   static StringBuilder output = new StringBuilder();
+   static StringTokenizer tokens;
+   static int[][] arr;
+   
+   public static void main(String[] args) throws NumberFormatException, IOException {
+      int N = Integer.parseInt(input.readLine());
+      arr = new int[N][2];
+      for(int i=0; i<N; i++) {
+         tokens = new StringTokenizer(input.readLine(), " ");
+         for(int j=0; j<2; j++) {
+            arr[i][j] = Integer.parseInt(tokens.nextToken());
+         }
+      }
+      Arrays.sort(arr, (o1, o2) -> {
+         if(o1[1]==o2[1]) {
+            return Integer.compare(o1[0], o2[0]);
+         }else {
+            return Integer.compare(o1[1], o2[1]);
+         }
+      });
+      for(int[] row: arr) {
+         output.append(row[0]+" "+row[1]+"\n");
+      }
+      System.out.println(output.toString());
+   }
+}
