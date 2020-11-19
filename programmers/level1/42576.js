2f845208dbebf6bdb3d6c1520c0fa0ea4f751b8a
JAEEUN JANG <jje141@gmail.com>
2020-11-19 13:07:40 -1000

    Update 42576.js

--- programmers/level1/42576.js
+++ programmers/level1/42576.js
@@ -8,3 +8,19 @@ function solution(participant, completion) {
     }
     return participant[participant.length-1]
 }
+
+// 20201119
+
+const solution = (participant, completion) => {
+    participant.sort()
+    completion.sort()
+    let idx = 0
+    while(idx < completion.length){
+        if(participant[idx] === completion[idx]){
+            idx ++
+        }else {
+            return participant[idx]
+        }
+    }
+    return participant[idx]
+}
