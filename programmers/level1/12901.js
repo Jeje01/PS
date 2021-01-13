926b84e67199a1d6a74bf68d835a338fa9951500
JAEEUN JANG <jje141@gmail.com>
2021-01-13 23:54:51 +0900

    2016년

--- programmers/level1/12901.js
+++ programmers/level1/12901.js
@@ -1,5 +1,6 @@
 // 2016년
 
+// 1 - 2020.05.06
 function solution(a, b) {
     var answer = '';
     var days = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
@@ -7,4 +8,15 @@ function solution(a, b) {
     var sum = a!=1 ? mon.slice(0, a-1).reduce((ac, cur) => ac+cur) : 0;
     answer = days[(sum+b-1)%7];
     return answer;
+}
+
+// 2 - 2021.01.13
+const solution = (a, b) => {
+    const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI" ,"SAT"]
+    const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30]
+    let total = 0
+    for(let i = 0; i < a - 1; i++) {
+        total += month[i]
+    }
+    return days[(total + b + 4) % 7]
 }
