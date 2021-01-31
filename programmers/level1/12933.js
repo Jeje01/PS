0105623c89b42e4346d4de0b103dd0e2e75cca84
JAEEUN JANG <jje141@gmail.com>
2021-01-31 23:25:35 +0900

    Update 12933.js

--- programmers/level1/12933.js
+++ programmers/level1/12933.js
@@ -1,6 +1,12 @@
 // 정수 내림차순으로 배치하기
 
+// 2020.05.13
 function solution(n) {
     var answer = n.toString().split("").sort().reverse().join("")*1;
     return answer;
+}
+
+// 2021.01.31
+const solution = (n) => {
+    return parseInt(n.toString().split("").sort().reverse().join(""))
 }
