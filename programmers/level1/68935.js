8d2c7776b4d9c0c0f1f4e1f37d9fe6697c80a250
JAEEUN JANG <jje141@gmail.com>
2021-02-06 21:23:02 -1100

    Update 68935.js

--- programmers/level1/68935.js
+++ programmers/level1/68935.js
@@ -1,25 +1,5 @@
 // 3진법 뒤집기
 
-const toDecimal = (n) => {
-    let mul = 1
-    let dec = 0
-    while (n!=0) {
-        dec += (n%10) * mul
-        n = Math.floor(n/10)
-        mul *= 3
-    }
-    return dec
-}
-
-const toBaseThree = (n) => {
-    let ret = ""
-    while(n!=0) {
-        ret += n%3
-        n = Math.floor(n/3)
-    }
-    return ret
-}
-
 const solution = (n) => {
-    return toDecimal(toBaseThree(n))
+    return parseInt(n.toString(3).split("").reverse().join(""), 3)
 }
