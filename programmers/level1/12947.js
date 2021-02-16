2e160355487d320fd3b7dd603eb0a8237a2e2ed2
JAEEUN JANG <jje141@gmail.com>
2021-02-16 22:58:03 +0900

    Update 12947.js

--- programmers/level1/12947.js
+++ programmers/level1/12947.js
@@ -5,3 +5,11 @@ function solution(x) {
     (x+"").split("").forEach(a => sum+=a*1)
     return x%sum==0
 }
+
+// 2021.02.16
+
+const solution = (x) => {
+    let sum = 0
+    x.toString().split("").forEach((x) => sum += Number.parseInt(x))
+    return x%sum === 0
+}
