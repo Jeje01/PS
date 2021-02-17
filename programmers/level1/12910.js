9b13cb2d3c2a305195191720961c69d8215a2eb9
JAEEUN JANG <jje141@gmail.com>
2021-02-17 22:49:32 +0900

    Update 12910.js

--- programmers/level1/12910.js
+++ programmers/level1/12910.js
@@ -4,3 +4,11 @@ function solution(arr, divisor) {
     var ans = arr.filter(el => el%divisor==0);
     return ans.length!=0? ans.sort((a, b) => a-b) : [-1];
 }
+
+// 2021.02.17
+
+const solution = (arr, divisor) => {
+    arr = arr.filter((num) => num % divisor === 0).sort((a, b) => a - b)
+    arr = arr.length === 0 ? [-1] : arr;
+    return arr
+}
