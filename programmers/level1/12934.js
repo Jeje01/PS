e39dfaae382e583b59b84170bba3ea423548b377
JAEEUN JANG <jje141@gmail.com>
2021-02-19 22:56:23 +0900

    Update 12934.js

--- programmers/level1/12934.js
+++ programmers/level1/12934.js
@@ -1,3 +1,9 @@
 // 정수 제곱근 판별
 
 const solution = (n) => (Math.sqrt(n) % 1 === 0 ? Math.pow(Math.sqrt(n)+1, 2) : -1)
+
+// 2021.02.19
+
+const solution = (n) => {
+    return Math.sqrt(n)%1 === 0 ? Math.pow(Math.sqrt(n)+1, 2) : -1;
+}
