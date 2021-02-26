c55978beed672f4135e40121e04ba74051e59c8a
JAEEUN JANG <jje141@gmail.com>
2021-02-26 22:52:35 +0900

    Update 12919.js

--- programmers/level1/12919.js
+++ programmers/level1/12919.js
@@ -4,3 +4,9 @@ function solution(seoul) {
     var answer = `김서방은 ${seoul.indexOf("Kim")}에 있다`;
     return answer;
 }
+
+// 2021.02.26
+
+const solution = (seoul) => {
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`
+}
