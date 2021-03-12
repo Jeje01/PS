7f3de0fb5ae778f1f5cb875c349e343dc248a162
JAEEUN JANG <jje141@gmail.com>
2021-03-12 20:03:04 +0900

    Update 12903.js

--- programmers/level1/12903.js
+++ programmers/level1/12903.js
@@ -11,3 +11,10 @@ function solution(s) {
     }
     return answer;
 }
+
+// 2021.03.12
+
+const solution = (s) => {
+    const arr = s.split("")
+    return arr.length % 2 === 0 ? arr[arr.length/2 - 1] + arr[arr.length/2] : arr[(arr.length + 1) / 2 - 1]
+}
