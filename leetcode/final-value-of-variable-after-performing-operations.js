f9bf89ce4823907953ca3260180aa039d2d9631e
JAEEUN JANG <jje141@gmail.com>
2022-02-12 18:13:08 +0900

    final-value-of-variable-after-performing-operations

---
+++ leetcode/final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,21 @@
+// 1
+/**
+ * Runtime: 87 ms, faster than 56.81% of JavaScript online submissions for Final Value of Variable After Performing Operations.
+ * Memory Usage: 44.1 MB, less than 5.63% of JavaScript online submissions for Final Value of Variable After Performing Operations.
+ */
+
+const finalValueAfterOperations = (operations) => {
+    const isIncrement = (str) => ["++X", "X++"].includes(str)
+    let answer = 0
+    operations.forEach((operation) => {
+        if (isIncrement(operation)) {
+            answer ++
+        } else {
+            answer --
+        }
+    })
+    return answer
+}
+
+
+// 2
