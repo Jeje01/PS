d76b6fdda574b28bb0df40d0acdca325ea5af73d
JAEEUN JANG <jje141@gmail.com>
2024-02-11 19:36:10 +0900

    240211 12939.js

--- programmers/level2/12939.js
+++ programmers/level2/12939.js
@@ -1,6 +1,15 @@
 // 최댓값과 최솟값
 
+// 20.06.15
 function solution(s) {
     var numbers = s.split(" ").map(a => a*1).sort((a, b) => a-b)
     return numbers[0]+" "+numbers[numbers.length-1]
+}
+
+// 24.02.11
+const solution = (s) => {
+    const numbers = s.split(" ")
+    const max = Math.max(...numbers)
+    const min = Math.min(...numbers)
+    return `${min} ${max}`
 }
