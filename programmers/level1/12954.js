5796216967d0c8c09e82726cda0f7d2d0b76a032
JAEEUN JANG <jje141@gmail.com>
2024-02-11 19:31:51 +0900

    240211 12954.js

--- programmers/level1/12954.js
+++ programmers/level1/12954.js
@@ -9,11 +9,19 @@ const solution = (x, n) => {
 }
 
 // 2021.03.06
-
 const solution = (x, n) => {
     const answer = []
     for (let i = 1; i <= n; i++) {
         answer.push(x*i)
+    }
+    return answer
+}
+
+// 2024.02.11
+const solution = (x, n) => {
+    const answer = [x]
+    for (let i = 1; i < n; i++) {
+        answer.push(answer[i - 1] + x)
     }
     return answer
 }
